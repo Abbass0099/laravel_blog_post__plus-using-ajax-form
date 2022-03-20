890f8472da0e4aed1c87c1b7dfa0e6ea58903ba1
Abbass <89728657+Abbass0099@users.noreply.github.com>
2022-03-20 17:03:12 +0200

    Add files via upload

---
+++ storage/framework/views/5fa9bf3678844caa88ee62e5f20c4633b202af4a.php
@@ -0,0 +1,48 @@
+<?php $__env->startSection('content'); ?>
+    <h1>Create Post</h1>
+    <?php echo Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']); ?>
+
+        <div class="form-group">
+            <?php echo e(Form::label('title', 'Title')); ?>
+
+            <?php echo e(Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])); ?>
+
+        </div>
+        <div class="form-group">
+            <?php echo e(Form::label('body', 'Body')); ?>
+
+            <?php echo e(Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])); ?>
+
+        </div>
+
+        <?php if(count($unique_posts) > 0): ?>
+            <div class="form-group row">    
+                <div class="col-sm-8">
+                    <select class="form-control" id="selectUser" name="user_selected" required focus>
+                        <option value="" disabled selected>Select title</option>                    
+                            <?php $__currentLoopData = $unique_posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $unique_post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+                                <option><?php echo e($unique_post->title); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>                        
+                    </select>
+                </div>        
+            </div>
+            <?php else: ?>
+                <p>no unique posts</p>
+        <?php endif; ?>
+
+        
+
+        <div class="form-group">
+            <?php echo e(Form::file('cover_image')); ?>
+
+        </div>
+        <?php echo e(Form::submit('Submit', ['class'=>'btn btn-primary'])); ?>
+
+    <?php echo Form::close(); ?>
+
+<?php $__env->stopSection(); ?>
+
+
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/b3c44901eb64e5bcdf8010d48772f5d92dce2c0b.php
@@ -0,0 +1,23 @@
+<?php $__env->startSection('content'); ?>
+    <div class="jumbotron text-center">
+        <h1>Welcome To Lsapp!</h1>
+        <!-- <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p> -->
+        <p> 
+
+            <?php if(Auth::check()): ?>
+                <!-- <a class="btn btn-primary btn-lg" href="/login" role="button" disabled>Login</a> 
+                <a class="btn btn-success btn-lg" href="/register" role="button" disabled>Register</a> -->
+
+                <p style="color:green; font-size:35px; font-weight:bold">You are logged In</p>
+
+            <?php else: ?>
+                <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> 
+                <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+            <?php endif; ?>
+
+            
+        </p>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
